aecbfde31b0ec5b7e5c2583052d4fc4806048b45
rkt-1597 <activprithvi@gmail.com>
2024-10-13 13:01:04 +0530

    Added boilerplate code for first assignment (Operators)

---
+++ Operators, Precedence and Expressions/A1-sum_of_digits.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+
+    //Take an integer as input
+
+
+    //Define a variable for storing the sum (~ 1 line)
+    
+    
+    //Use while loop to chck whether number is greater than zero. 
+    //Check each digit of number and add it to form total
+    
+    
+    //Print the sum of the digits of given number
+    
+    
+    return 0;
+}
