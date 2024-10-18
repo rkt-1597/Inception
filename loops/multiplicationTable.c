6ee52d02a6658c2f835472fe2f7b7a1cf445d267
sneha31415 <sneha.singh.31415@gmail.com>
2024-10-18 23:30:34 +0530

    added the questions along with the boiler plate for loops

---
+++ loops/multiplicationTable.c
@@ -0,0 +1,17 @@
+# include<stdio.h>
+/*
+print the multiplication table of a number of your choice
+eg : 7
+7 X 1 = 7 
+7 X 2 = 14
+.
+.
+.
+7 X 10 = 70 
+*/ 
+int main(){
+    // write your code here
+
+
+    return 0;
+}
