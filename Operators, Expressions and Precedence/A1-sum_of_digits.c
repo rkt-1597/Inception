b5220a57178f9aacb9f2abf3ffe91e522a4ed685
rkt-1597 <activprithvi@gmail.com>
2024-10-18 20:24:51 +0530

    Refined codes for operators, expressions and precedence

---
+++ Operators, Expressions and Precedence/A1-sum_of_digits.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/*
+Checking the sum of the digits of given integer
+
+Steps :
+1. Define variables for original number (orig_num), digit 
+of number (digit) and another variable for storing sum of
+digits (sum)
+
+2. Take an integer from user as input and store it in orig_num
+
+3. Iterate over each digit of the orig_num, using digit and 
+orig_num variables, and form the sum by adding each digit
+
+4. print the sum of the digits of given number
+*/
+
+int main(){
+
+    //Define required variables
+    int orig_num, digit, sum;
+    
+    //Your code below
+    
+    
+    return 0;
+}
+
+
