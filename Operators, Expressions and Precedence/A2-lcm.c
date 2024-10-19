d7e1d0e9b192046f0f311cc079445fc25a2a867a
rkt-1597 <activprithvi@gmail.com>
2024-10-19 13:15:03 +0530

    Added new assignment questions

---
+++ Operators, Expressions and Precedence/A2-lcm.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/*
+Finding the LCM of 2 numbers
+
+Steps :
+1. Define variables for 2 numbers (a and b) and their LCM (lcm)
+
+2. Take 2 integers from user as input and store them in a and b
+
+3. Store the value of the highest among given 2 numbers, in 'lcm'
+
+4. Iterate infinitely, such that in each iteration, if value of lcm is
+divisible by the given 2 numbers, then print the value of lcm and leave 
+the loop 
+*/
+
+int main(){
+
+    //Define required variables
+    int a, b, lcm;
+    
+    //Your code below
+    
+    
+    return 0;
+}
