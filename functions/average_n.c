142bc3cf9ced994e2566c77fcccceea1d2ee12d7
Aaditya J <ajoil4068@gmail.com>
2024-10-19 17:07:22 +0530

    Functions and Its related code

---
+++ functions/average_n.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+void function_name(){
+	// Your code for a function should go here
+}
+
+int main()
+{
+	// This is the data for the first test case
+	float array[] = {1.2, 4.1, 5.16, 7.2, 11};
+	int length = 5;
+
+	// Your code should go here
+
+	return 0;
+}
---
+++ functions/functions.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+void function_name(){
+	// Your code for a function should go here
+}
+
+int main()
+{
+	// Your code should go here
+
+	return 0;
+}
