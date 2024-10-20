16f01536c1485392b26e3c8a04c86c7cd4afe220
Aaditya J <ajoil4068@gmail.com>
2024-10-20 10:17:47 +0530

    Added boilerplate code

---
+++ functions/nesting_functions.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// Function declarations
+void functionName1();
+void functionName2();
+
+int main() {
+    // Code to initialize variables or take input
+    
+    // Calling functions
+    functionName1();
+    functionName2();
+
+    return 0;
+}
+
+// Function definition for functionName1
+void functionName1() {
+    // Code to perform the task for functionName1
+}
+
+// Function definition for functionName2
+void functionName2() {
+    // Code to perform the task for functionName2
+}
